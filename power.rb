2ff05114de07216de0427250876bc0a44076364c
Tommy Rodriguez <trodriguez91@icloud.com>
2015-03-30 12:05:01 -0700

    Solve Problem 1

---
+++ power.rb
@@ -0,0 +1,11 @@
+# return base raised to the power of the exponent without '**'
+def power( base, exponent )
+	total = 1
+	until exponent == 0
+	  total *= base
+	  exponent -= 1
+	end
+	puts total
+end
+
+power( 2, 8 )
