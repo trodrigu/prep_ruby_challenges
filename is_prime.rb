34fba298dfca722d1f64d6931f2dde7b03363d68
Tommy Rodriguez <trodriguez91@icloud.com>
2015-03-31 08:54:48 -0700

    Delete second half array addition
    After further inspection the second half was limiting and unnecessary.

--- is_prime.rb
+++ is_prime.rb
@@ -5,9 +5,7 @@ def is_prime?( number )
 end
 
 def check_arr( number )
-	first_half_check_arr = (2..number-1).to_a
-	second_half_check_arr = (number+1..9).to_a
-	check_arr = first_half_check_arr + second_half_check_arr
+	check_arr = (2..number-1).to_a
 	check_arr.map { |a| number % a }
 end
 
